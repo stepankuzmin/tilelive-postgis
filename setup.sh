df08f0a48bcb46f62b035e9b99dcc441a396d32d
Stepan Kuzmin <to.stepan.kuzmin@gmail.com>
2017-04-18 18:59:18 +0300

    hotfix

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-apt-get update -yqq
-apt-get install python zlib1g-dev clang make pkg-config curl
+sudo apt-get update -yqq
+sudo apt-get install python zlib1g-dev clang make pkg-config curl
 
 mkdir ./mason
 curl -sSfL https://github.com/mapbox/mason/archive/v0.9.0.tar.gz | tar --gunzip --extract --strip-components=1 --exclude="*md" --exclude="test*" --directory=./mason
