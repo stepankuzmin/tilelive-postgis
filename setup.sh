847266ed069f74f364d6e8a321655a0004b6bdbb
Stepan Kuzmin <to.stepan.kuzmin@gmail.com>
2017-04-19 14:37:25 +0300

    update mapnik to v3.6.0

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir ./mason
-curl -sSfL https://github.com/mapbox/mason/archive/v0.9.0.tar.gz | tar --gunzip --extract --strip-components=1 --exclude="*md" --exclude="test*" --directory=./mason
-
-./mason/mason install mapnik latest
-./mason/mason link mapnik latest
-
-export PATH=$PATH:$(pwd)/mason_packages/.link/bin
